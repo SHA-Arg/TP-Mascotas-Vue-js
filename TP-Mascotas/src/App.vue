58d62d696867b4954e53e61afe1549d5e6527014
Seba HA <hereniu.amaral.sebastian@gmail.com>
2023-10-18 01:41:06 -0300

    Up Vuejs Mascotas

---
+++ TP-Mascotas/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const name = "Vue dinamico";
+const styleColor = "color: red";
+</script>
+
+<template>
+	<h1>Hola {{ name }}</h1>
+	<h2 :style="styleColor">Soy Rojo</h2>
+</template>
+
+<style>
+h1 {
+	color: blue;
+}
+</style>
